5422c2d2f2722d093a5dafdc3938408a6a4ccc45
Robin KERJEAN <rokerjea@e1r2p19.42.fr>
2021-07-23 21:07:53 +0200

    join and trim

---
+++ ft_strtrim.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokerjea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/23 16:53:24 by rokerjea          #+#    #+#             */
+/*   Updated: 2021/07/23 20:56:00 by rokerjea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include <stdio.h>
+int	ft_isinstr(char c, const char *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*res;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s1[j])
+		j++;
+	printf ("len is %i \n", j);
+	while (ft_isinstr(s1[i], set))
+		i++;
+	printf ("start is %i \n", i);
+	j--;
+	while (ft_isinstr(s1[j], set))
+		j--;
+	printf ("end is %i \n", j);
+	
+	printf ("newlen is %i \n", (j - i));
+	res = malloc(sizeof(char) * (j - i + 1));
+	while (i <= j)
+	{
+		res[k] = s1[i];
+		i++;
+		k++;
+	}
+	res[k] = '\0';
+	return (res);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char str[19] = "abbcpouetccbbaa";
+	char set[3] = "abc";
+	printf ("%s\n", ft_strtrim(str, set));
+	return (0);
+}
