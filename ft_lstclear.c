a3e4cc4c8f94f5b6975e8bb14b7c986c78b147de
Robin KERJEAN <rokerjea@e1r2p19.42.fr>
2021-08-11 19:17:52 +0200

    ok

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: rokerjea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 18:22:47 by rokerjea          #+#    #+#             */
-/*   Updated: 2021/08/11 19:00:42 by rokerjea         ###   ########.fr       */
+/*   Updated: 2021/08/11 19:14:55 by rokerjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list *maillon;
+	t_list *maillon2;
 
 	maillon = *lst;
+	maillon2 = maillon;
 	while(maillon)
 	{
 	ft_lstdelone(maillon, del);
